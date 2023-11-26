ea1226dd886b9fd8e2806f4e9b593130876d0178
dlwlstn3585 <dlwlstn3585@naver.com>
2023-11-26 18:35:51 +0900

    second

---
+++ src/release.h
@@ -0,0 +1,5 @@
+#define REDIS_GIT_SHA1 "00000000"
+#define REDIS_GIT_DIRTY "0"
+#define REDIS_BUILD_ID "embedded-virtual-machine-1700470809"
+#include "version.h"
+#define REDIS_BUILD_ID_RAW REDIS_VERSION REDIS_BUILD_ID REDIS_GIT_DIRTY REDIS_GIT_SHA1
